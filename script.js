2da9c2e4f4637e1ddae59a90eb2f247e1691e7bc
Pranavi Singh <138822333+pranavisingh8@users.noreply.github.com>
2024-08-05 13:30:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var typed = new Typed('#element', {
-    strings: ['Web Developer', 'Web Designer', 'Game Developer', 'Video Editor'],
-    typeSpeed: 50,
-});
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    <script>
+        var typed = new Typed('#element', {
+            strings: ['Web Developer', 'Web Designer', 'Game Developer', 'Video Editor'],
+            typeSpeed: 50,
+        });
+    </script>
